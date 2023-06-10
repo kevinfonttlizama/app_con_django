e1a89c49f8fea1ea428875c16b9c0dc267a8b448
kevin <kevin,andresfontt@gmail.com>
2023-06-10 04:42:16 -0400

    operaciones base de datos

--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -13,6 +13,8 @@ from django.db import models
 
 #el numero de migracion ayuda a verificar el numero de migraciones que hemos en la base de datos y los cambios que hemos hecho en ella
 
+#python3 manage.py migrate con esto le decimos a python que utilice el codigo sql que acabamos de generar para finalmente tener las tablas dentro de la base de datos
+
 
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
@@ -30,3 +32,31 @@ class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
+
+
+#insertar registros from gestionPedidos.models import Articulos
+#art=Articulos(nombre='MESA', seccion='decoracion', precio=90) creando un objeto para insertarlo en ta tabla si nos damos cuenta usamos las variables definidas en la clase articulo para construir el articulo segun su modelo en la base de datos
+#art.save() ejecutar la consulta sql
+
+#art3=Articulos.objects.create(nombre='TALADRO',#seccion='FERRETERIA', precio=65) metodo para agregar-insertar con un solo comando osea sin el .save()
+
+
+#>>> art.precio=95
+#>>> art.save() <--- operacion update, cambiado el precio del articulo
+
+
+#>>> art5=Articulos.objects.get(id=3)   
+#>>> art5.delete() <--- operacion delete borrando un articulo por su id
+
+
+# >>> Lista=Articulos.objects.all()
+# >>> Lista
+# <QuerySet [<Articulos: Articulos object (1)>, <Articulos: Articulos object (2)>]>
+# 
+# >>> Lista.query.__str__()
+# 'SELECT "gestionPedidos_articulos"."id", "gestionPedidos_articulos"."nombre", "gestionPedidos_articulos"."seccion", "gestionPedidos_articulos"."precio" FROM "gestionPedidos_articulos"'
+
+#operacion select para la base de datos, que nos devuleve una lista con los articulos dentro de ella 
+
+
+
