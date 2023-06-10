78f54ab2544802eef7726845a84deb0e8698a3ee
kevin <kevin,andresfontt@gmail.com>
2023-06-10 06:02:41 -0400

    metodo __str__, consultas y comentarios

--- tiendaOnline/gestionPedidos/models.py
+++ tiendaOnline/gestionPedidos/models.py
@@ -27,10 +27,18 @@ class Clientes(models.Model):
 # >>> cli=Clientes(nombre='kevin', direccion='mi casita', email='kevin.andresfontt@gmail.com', telefono='1234332223')
 # >>> cli.save()
 
+
+#Importante siempre que hagamos cambios en los modelos debemos hacer las migraciones correspondientes. makemigrations - migrate
+
+#el metodo __str__ lo que hace es convertir a una cadena de caracteres legible  
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=30)
     precio=models.IntegerField()
+
+    def __str__(self):
+        return 'EL nombre es %s la seccion es %s y el precio es %s'%(self.nombre,self.seccion,self.precio)
+
 
 class Pedidos(models.Model):
     numero=models.IntegerField()
@@ -54,13 +62,34 @@ class Pedidos(models.Model):
 
 
 # >>> Lista=Articulos.objects.all()
-# >>> Lista
+# >>> Lista <-- queda guardado en la variable lista
+# y nos devuelve lo siguiente por consola
 # <QuerySet [<Articulos: Articulos object (1)>, <Articulos: Articulos object (2)>]>
-# 
+# con esto le pedimos que nos muestre lo que guardo dentro de la variable Lista que creamos y devuelve lo siguiente : 
 # >>> Lista.query.__str__()
 # 'SELECT "gestionPedidos_articulos"."id", "gestionPedidos_articulos"."nombre", "gestionPedidos_articulos"."seccion", "gestionPedidos_articulos"."precio" FROM "gestionPedidos_articulos"'
 
 #operacion select para la base de datos, que nos devuleve una lista con los articulos dentro de ella 
+
+#>>> python3 manage.py shell
+#>>> from gestionPedidos.models import Articulos
+#>>> Articulos.objects.filter(seccion='DEPORTES')
+#esto nos devolveria todo lo que esta dentro de la seccion deportes filtrado
+#<QuerySet []> me devuelve una lista vacia ya que no he introducido articulos en la base de datos 
+
+
+# >>> Articulos.objects.filter(seccion='MUEBLES') <--- cuando le hacemos esta consulta a la base de datos por consola le decimos que nos filtre por la seccion muebles de la tabla articulos de la base de datos, si tenemos configurado el metodo __str__ nos devuelve la siguiente cadena por cada objeto que hayamos filtrado, en este caso el metodo __str__ nos devuelve la cadena --> return 'EL nombre es %s la seccion es %s y el precio es %s'%(self.nombre,self.seccion,self.precio) <--
+
+#Entonces nos entrega esto por consola (lo de abajo)
+#                         
+# <QuerySet [<Articulos: EL nombre es SILLA la seccion es MUEBLES y el precio es 30>, <Articulos: EL nombre es MESA la seccion es MUEBLES y el precio es 80>, <Articulos: EL nombre es VELADOR la seccion es MUEBLES y el precio es 40>]>
+
+#  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#            salida de la consola 
+# 
+
+
+
 
 
 
